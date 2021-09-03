718fa34c99a08c107afb90056297eb24ee09ab87
CrowOnslaught <Crowblackclaws@gmail.com>
2021-09-04 01:12:09 +0200

    fix: addToDeckModal on cards with >4 maxAmount

--- src/components/Decks/AddToDeck.vue
+++ src/components/Decks/AddToDeck.vue
@@ -4,12 +4,12 @@
       Add 1 to {{ currentDeck.name }}
     </ion-button>
   </ion-item>
-  <ion-item class="optionDeckItem" v-if="openSlotsInDeck >= 2">
+  <ion-item class="optionDeckItem" v-if="openSlotsInDeck >= 2 && !(openSlotsInDeck<= (cardMaxAmount-3))">
     <ion-button fill="clear" @click="addToDeck(2)">
       Add 2 to {{ currentDeck.name }}
     </ion-button>
   </ion-item>
-  <ion-item class="optionDeckItem" v-if="openSlotsInDeck >= 3">
+  <ion-item class="optionDeckItem" v-if="openSlotsInDeck >= 3 && !(openSlotsInDeck<= (cardMaxAmount-2))">
     <ion-button fill="clear" @click="addToDeck(3)">
       Add 3 to {{ currentDeck.name }}
     </ion-button>
@@ -19,22 +19,22 @@
       Add 4 to {{ currentDeck.name }}
     </ion-button>
   </ion-item>
-  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= 3">
+  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= (cardMaxAmount-1)">
     <ion-button fill="clear" @click="removeFromDeck(1)">
       Remove 1 from {{ currentDeck.name }}
     </ion-button>
   </ion-item>
-  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= 2">
+  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= (cardMaxAmount-2) && !(openSlotsInDeck >=3)">
     <ion-button fill="clear" @click="removeFromDeck(2)">
       Remove 2 from {{ currentDeck.name }}
     </ion-button>
   </ion-item>
-  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= 1">
+  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= (cardMaxAmount-3)  && !(openSlotsInDeck >=2)">
     <ion-button fill="clear" @click="removeFromDeck(3)">
       Remove 3 from {{ currentDeck.name }}
     </ion-button>
   </ion-item>
-  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= 0">
+  <ion-item class="optionDeckItem" v-if="openSlotsInDeck <= (cardMaxAmount-4)">
     <ion-button fill="clear" @click="removeFromDeck(4)">
       Remove 4 from {{ currentDeck.name }}
     </ion-button>
@@ -52,6 +52,7 @@
       Close
     </ion-button>
   </ion-item>
+
 </template>
 
 <script>
@@ -71,7 +72,7 @@ export default {
   components: { IonItem, IonButton, IonSelect, IonSelectOption, IonLabel },
   data() {
     return {
-      openSlotsInDeck: 4,
+      openSlotsInDeck: this.cardMaxAmount,
       selectedSet: this.card.setCode[0],
     };
   },
@@ -84,10 +85,18 @@ export default {
       currentDeck: (state) => state.decks.currentDeck,
     }),
     cardIdCode: function() {
-      if (this.card.id.includes("|")) return this.card.id;
-
+      if (this.card.id.includes("|")) 
+        return this.card.id;
       return this.card.id + "|" + this.selectedSet;
     },
+    cardMaxAmount: function(){
+      switch(this.card.id.split("|")[0]){
+        case "493":
+          return 16;
+        default:
+          return 4;
+      }
+    }
   },
   watch: {
     selectedSet: function() {
@@ -110,8 +119,8 @@ export default {
       const cardSlot = this.currentDeck.decklist.find(
         (e) => e.cardId == this.cardIdCode
       );
-      if (cardSlot == undefined) this.openSlotsInDeck = 4;
-      else this.openSlotsInDeck = 4 - cardSlot.amount;
+      if (cardSlot == undefined) this.openSlotsInDeck = this.cardMaxAmount;
+      else this.openSlotsInDeck = this.cardMaxAmount - cardSlot.amount;
     },
     close() {
       modalController.dismiss({ flag: true });
